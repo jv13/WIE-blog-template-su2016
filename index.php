749ffd28b170bba6a5ac9d8bbc639c008c60db65
Jen Vitrano <vitranojen@gmail.com>
2016-07-01 09:55:09 -0400

    Add font and styling to  headers

--- index.php
+++ index.php
@@ -18,10 +18,11 @@
                         }
                       ?>
 
-                      <h4><?php the_date(); ?></h4>
-                      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                      <h4 class="postdate"><?php the_date(); ?></h4>
 
-                      <h4><?php the_tags(); ?></h4>
+                      <h3 class="posttitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+                      <h4 class="posttags"><?php the_tags(); ?></h4>
 
                     </div>
                   <?php
