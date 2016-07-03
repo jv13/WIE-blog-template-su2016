0c2b74824f2c29c4779fa7aed7d48b0af04a8f92
Jen Vitrano <vitranojen@gmail.com>
2016-07-03 18:47:50 -0400

    Update contact form

---
+++ thank-you.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Jen Vitrano Blog Site</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheets/skeleton.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+
+          <div class="twelve columns">
+              <h3>Thanks!</h3>
+              <p>Thank you for filling out this form, I will be in touch shortly.</p>
+              <p><a href="index.php">Return to the homepage</a></p>
+          </div>
+
+      </div>
+    </div>
+  </body>
+</html>
