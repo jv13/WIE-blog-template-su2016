69052664f076ebfe57e213e0b4717f3407afa5bc
Jen Vitrano <vitranojen@gmail.com>
2016-07-03 21:16:50 -0400

    Update to share in post page

--- single.php
+++ single.php
@@ -18,7 +18,10 @@
                     <h2 id="posttitle"><?php the_title(); ?></h2>
                     <?php the_content(); ?>
 
+                  <!-- Hupso Share Buttons - http://www.hupso.com/share/ --><div class="hupso-share-buttons"><a class="hupso_toolbar" href="http://www.hupso.com/share/"><img src="//static.hupso.com/share/buttons/share-medium.png" style="border:0px; padding-top:5px; float:left;" alt="Share Button"/></a><script type="text/javascript">var hupso_services_t=new Array("Twitter","Facebook","Google Plus","Pinterest","Linkedin","Email","Print");var hupso_toolbar_size_t="medium";</script><script type="text/javascript" src="//static.hupso.com/share/js/share_toolbar.js"></script></div><!-- Hupso Share Buttons -->
+
                     <p class="signature"> -- jen </p>
+
 
                     <?php
                   } // end while
@@ -44,7 +47,7 @@
               $args=array(
               'tag__in' => $tag_ids,
               'post__not_in' => array($post->ID),
-              'posts_per_page'=>1, // Number of related posts to display.
+              'posts_per_page'=>3, // Number of related posts to display.
               'caller_get_posts'=>1
               );
 
@@ -72,86 +75,9 @@
 
 
 
-<div class="four columns">
-<div class="relatedposts">
-  <h3>Related posts</h3>
-    <?php
-      $orig_post = $post;
-      global $post;
-      $tags = wp_get_post_tags($post->ID);
-
-      if ($tags) {
-        $tag_ids = array();
-        foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
-        $args=array(
-        'tag__in' => $tag_ids,
-        'post__not_in' => array($post->ID),
-        'posts_per_page'=>1, // Number of related posts to display.
-        'caller_get_posts'=>1
-        );
-
-        $my_query = new wp_query( $args );
-
-        while( $my_query->have_posts() ) {
-          $my_query->the_post();
-      ?>
-
-<div class="relatedthumb">
-    <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail('medium'); ?><br />
-    <h4 class="postdate"><?php the_time('F j, Y'); ?> </h4>
-    <h3><?php the_title(); ?></h3>
-    <h4><?php the_tags(); ?></h4>
-    </a>
-</div>
-
-<?php }
-      }
-$post = $orig_post;
-wp_reset_query();
-?>
-</div>
-</div>
 
 
-<div class="four columns">
-<div class="relatedposts">
-  <h3>Related posts</h3>
-    <?php
-      $orig_post = $post;
-      global $post;
-      $tags = wp_get_post_tags($post->ID);
 
-      if ($tags) {
-        $tag_ids = array();
-        foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
-        $args=array(
-        'tag__in' => $tag_ids,
-        'post__not_in' => array($post->ID),
-        'posts_per_page'=>1, // Number of related posts to display.
-        'caller_get_posts'=>1
-        );
-
-        $my_query = new wp_query( $args );
-
-        while( $my_query->have_posts() ) {
-          $my_query->the_post();
-      ?>
-
-<div class="relatedthumb">
-    <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail('medium'); ?><br />
-    <h4 class="postdate"><?php the_time('F j, Y'); ?> </h4>
-    <h3><?php the_title(); ?></h3>
-    <h4><?php the_tags(); ?></h4>
-    </a>
-</div>
-
-<?php }
-      }
-$post = $orig_post;
-wp_reset_query();
-?>
-</div>
-</div>
     </section>
   </div>
 
