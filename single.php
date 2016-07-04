1d29440d204a9ecf0644e9a485c3d91d2ca8783f
Jen Vitrano <vitranojen@gmail.com>
2016-07-03 21:05:04 -0400

    Post page update columns

--- single.php
+++ single.php
@@ -30,6 +30,7 @@
     </section>
 
     <section class="row">
+      <div class="four columns">
       <div class="relatedposts">
         <h3>Related posts</h3>
           <?php
@@ -43,7 +44,7 @@
               $args=array(
               'tag__in' => $tag_ids,
               'post__not_in' => array($post->ID),
-              'posts_per_page'=>3, // Number of related posts to display.
+              'posts_per_page'=>1, // Number of related posts to display.
               'caller_get_posts'=>1
               );
 
@@ -67,7 +68,90 @@
       wp_reset_query();
       ?>
   </div>
+</div>
 
+
+
+<div class="four columns">
+<div class="relatedposts">
+  <h3>Related posts</h3>
+    <?php
+      $orig_post = $post;
+      global $post;
+      $tags = wp_get_post_tags($post->ID);
+
+      if ($tags) {
+        $tag_ids = array();
+        foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+        $args=array(
+        'tag__in' => $tag_ids,
+        'post__not_in' => array($post->ID),
+        'posts_per_page'=>1, // Number of related posts to display.
+        'caller_get_posts'=>1
+        );
+
+        $my_query = new wp_query( $args );
+
+        while( $my_query->have_posts() ) {
+          $my_query->the_post();
+      ?>
+
+<div class="relatedthumb">
+    <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail('medium'); ?><br />
+    <h4 class="postdate"><?php the_time('F j, Y'); ?> </h4>
+    <h3><?php the_title(); ?></h3>
+    <h4><?php the_tags(); ?></h4>
+    </a>
+</div>
+
+<?php }
+      }
+$post = $orig_post;
+wp_reset_query();
+?>
+</div>
+</div>
+
+
+<div class="four columns">
+<div class="relatedposts">
+  <h3>Related posts</h3>
+    <?php
+      $orig_post = $post;
+      global $post;
+      $tags = wp_get_post_tags($post->ID);
+
+      if ($tags) {
+        $tag_ids = array();
+        foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+        $args=array(
+        'tag__in' => $tag_ids,
+        'post__not_in' => array($post->ID),
+        'posts_per_page'=>1, // Number of related posts to display.
+        'caller_get_posts'=>1
+        );
+
+        $my_query = new wp_query( $args );
+
+        while( $my_query->have_posts() ) {
+          $my_query->the_post();
+      ?>
+
+<div class="relatedthumb">
+    <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail('medium'); ?><br />
+    <h4 class="postdate"><?php the_time('F j, Y'); ?> </h4>
+    <h3><?php the_title(); ?></h3>
+    <h4><?php the_tags(); ?></h4>
+    </a>
+</div>
+
+<?php }
+      }
+$post = $orig_post;
+wp_reset_query();
+?>
+</div>
+</div>
     </section>
   </div>
 
